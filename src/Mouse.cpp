ace719ef66867e90831c27fdb764c0f5b07363f2
lukakvesic <luka.kvesic@fer.hr>
2024-10-30 22:44:54 +0100

    Update Mouse.cpp

--- src/Mouse.cpp
+++ src/Mouse.cpp
@@ -34,15 +34,15 @@ static const uint8_t _hidReportDescriptor[] PROGMEM = {
     0x85, 0x01,                    //     REPORT_ID (1)
     0x05, 0x09,                    //     USAGE_PAGE (Button)
     0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x05,                    //     USAGE_MAXIMUM (Button 5)  // Promijenjeno na 5
+    0x29, 0x05,                    //     USAGE_MAXIMUM (Button 5)  // Changed to 5
     0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
     0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x05,                    //     REPORT_COUNT (5)  // Promijenjeno na 5
+    0x95, 0x05,                    //     REPORT_COUNT (5)  // Changed to 5
     0x75, 0x01,                    //     REPORT_SIZE (1)
     0x81, 0x02,                    //     INPUT (Data,Var,Abs)
     0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x03,                    //     REPORT_SIZE (3)  // Promijenjeno na 3
-    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)  // Promijenjeno na Cnst
+    0x75, 0x03,                    //     REPORT_SIZE (3)  // Changed to 3
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)  // Changed to Cnst
     0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
     0x09, 0x30,                    //     USAGE (X)
     0x09, 0x31,                    //     USAGE (Y)
